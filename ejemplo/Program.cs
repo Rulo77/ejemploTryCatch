4de6cf9da664d1b4693534027f9c4e81e9b83235
Raul Gomez <ragomez1234567@gmail.com>
2021-10-06 15:48:02 -0600

    Add project files.

---
+++ ejemplo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             string nombre="Raul";
+             string apellido="Gomez";
+              int año=22;
+       
+            int age;
+            try
+            {
+                if (año < 21)
+                    throw new ArgumentOutOfRangeException("año", "todos los invitados deven de tener mas de 21 años");
+                else
+                    age = año;
+
+                Console.WriteLine(nombre + " " + apellido + ", " + año);
+            }
+            catch (ArgumentOutOfRangeException outOfRange)
+            {
+
+                Console.WriteLine("Error: {0}", outOfRange.Message);
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
